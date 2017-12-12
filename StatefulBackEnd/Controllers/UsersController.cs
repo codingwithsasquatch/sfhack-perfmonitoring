08db67d8c75831c73138eeb634dca5ec57223129
Megan Meyer <memeyer@microsoft.com>
2017-12-11 17:38:33 -0800

    Initial cut at stateful back end. Implements get and get by ID.

---
+++ StatefulBackEnd/Controllers/UsersController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using Models;
+
+namespace StatefulBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IReliableStateManager stateManager;
+
+        public UsersController(IReliableStateManager stateManager)
+        {
+            this.stateManager = stateManager;
+        }
+
+        //// GET api/users
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<KeyValuePair<long, User>> result = new List<KeyValuePair<long, User>>();
+
+                ConditionalValue<IReliableDictionary<long, User>> tryGetResult =
+                    await this.stateManager.TryGetAsync<IReliableDictionary<long, User>>(StatefulBackEnd.UsersDictionaryName);
+
+                if (tryGetResult.HasValue)
+                {
+                    IReliableDictionary<long, User> dictionary = tryGetResult.Value;
+
+                    using (ITransaction tx = this.stateManager.CreateTransaction())
+                    {
+                        IAsyncEnumerable<KeyValuePair<long, User>> enumerable = await dictionary.CreateEnumerableAsync(tx);
+                        IAsyncEnumerator<KeyValuePair<long, User>> enumerator = enumerable.GetAsyncEnumerator();
+
+                        while (await enumerator.MoveNextAsync(CancellationToken.None))
+                        {
+                            result.Add(enumerator.Current);
+                        }
+                    }
+                }
+                return this.Json(result);
+            }
+            catch (FabricException)
+            {
+                return new ContentResult { StatusCode = 503, Content = "The service was unable to process the request. Please try again." };
+            }
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(long id)
+        {
+            try
+            {
+                IReliableDictionary<long, User> usersDictionary =
+                    await this.stateManager.GetOrAddAsync<IReliableDictionary<long, User>>(StatefulBackEnd.UsersDictionaryName);
+
+                using (ITransaction tx = this.stateManager.CreateTransaction())
+                {
+                    ConditionalValue<User> result = await usersDictionary.TryGetValueAsync(tx, id);
+
+                    if (result.HasValue)
+                    {
+                        return this.Ok(result.Value);
+                    }
+
+                    return this.NotFound();
+                }
+            }
+            catch (FabricNotPrimaryException)
+            {
+                return new ContentResult { StatusCode = 410, Content = "The primary replica has moved.  Please re-resolve the service." };
+            }
+            catch (FabricException)
+            {
+                return new ContentResult { StatusCode = 503, Content = "The service was unable to process the request. Please try again." };
+            }
+        }
+    }
+}
